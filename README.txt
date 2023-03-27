f0243fd17c2a3899211f9fae670371238c35f8d7
ohtero <91533120+Tuhto@users.noreply.github.com>
2023-03-27 15:41:48 +0300

    initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Guestbook app
+
+testing
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const axios = require('axios')
+const PORT = process.env.PORT || 3000;
+
+const fs = require("fs");
+
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+app.get("/guestbook", (req, res) => {
+  const messages = require("./messages.json");
+  res.render("pages/guestbook", { messages: messages });
+});
+
+app.get("/newmessage", (req, res) => {
+  res.render("pages/newmessage");
+});
+
+app.post("/newmessage", urlencodedParser, (req, res) => {
+  class Message {
+    constructor(name, country, message) {
+      this.name = name;
+      this.country = country;
+      this.message = message;
+    }
+  }
+  const savedMessages = require("./messages.json");
+  const newMessage = new Message(
+    req.body.name,
+    req.body.country,
+    req.body.message
+  );
+  savedMessages.unshift(newMessage);
+  fs.writeFileSync("./messages.json", JSON.stringify(savedMessages));
+  res.render("pages/newmessage");
+});
+
+app.get("/ajaxmessage", (req, res) => {
+  res.render("pages/ajaxmessage")
+});
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+
